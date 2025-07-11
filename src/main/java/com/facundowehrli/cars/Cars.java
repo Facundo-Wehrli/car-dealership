802baea6102cadebc37bf388b45ae14d1b3dfca2
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-07-11 18:38:16 -0300

    initial project structure

---
+++ src/main/java/com/facundowehrli/cars/Cars.java
@@ -0,0 +1,9 @@
+package com.facundowehrli.cars;
+
+
+public class Cars {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/facundowehrli/cars/service/Car.java
@@ -0,0 +1,82 @@
+package com.facundowehrli.cars.service;
+
+public class Car {
+
+    private int id;
+    private String model;
+    private String brand;
+    private String engine;
+    private String licencePlate;
+    private String color;
+    private int doorCount;
+
+    public Car() {
+    }
+
+    public Car(int id, String model, String brand, String engine, String licencePlate, String color, int doorCount) {
+        this.id = id;
+        this.model = model;
+        this.brand = brand;
+        this.engine = engine;
+        this.licencePlate = licencePlate;
+        this.color = color;
+        this.doorCount = doorCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoorCount() {
+        return doorCount;
+    }
+
+    public void setDoorCount(int doorCount) {
+        this.doorCount = doorCount;
+    }
+
+}
