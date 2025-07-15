dbc2086d144dfb09312d46bd2ac98195177406aa
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-07-15 14:41:42 -0300

    feat: new method addCar that communicates gui and persistence layers

---
+++ src/main/java/com/facundowehrli/car/service/Controller.java
@@ -0,0 +1,22 @@
+package com.facundowehrli.car.service;
+
+import com.facundowehrli.car.persistence.PersistenceController;
+
+public class Controller {
+
+    PersistenceController controlPersis = new PersistenceController();
+
+    public void addCar(String model, String brand, String engine, String licencePlate, String color, int doorCount) {
+        //create a new Car befor saving it on persistence layer
+        Car car = new Car();
+        car.setModel(model);
+        car.setBrand(brand);
+        car.setEngine(engine);
+        car.setColor(color);
+        car.setLicencePlate(licencePlate);
+        car.setDoorCount(doorCount);
+
+        controlPersis.addCar(car);
+    }
+
+}
