828b9401243a9d36f8dc54567bedccb4bdd2d82d
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-07-18 17:28:28 -0300

    feat: service layer asking for data to persitence layer through jpa methods

--- src/main/java/com/facundowehrli/car/persistence/PersistenceController.java
+++ src/main/java/com/facundowehrli/car/persistence/PersistenceController.java
@@ -5,19 +5,31 @@ import com.facundowehrli.car.service.Car;
 import java.util.List;
 
 public class PersistenceController {
-    
+
     CarJpaController carJpa = new CarJpaController();
-    
+
     public void addCar(Car car) {
         carJpa.create(car);
     }
-    
+
     public List<Car> getCars() {
         return carJpa.findCarEntities();
     }
-    
+
     public void deleteCar(int idCar) throws NonexistentEntityException {
         carJpa.destroy(idCar);
     }
-    
+
+    public Car getCar(int idCar) {
+        return carJpa.findCar(idCar);
+    }
+
+    public void editCar(Car car) {
+        try {
+            carJpa.edit(car);
+        } catch (Exception ex) {
+            System.getLogger(PersistenceController.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+
 }
--- src/main/java/com/facundowehrli/car/service/Controller.java
+++ src/main/java/com/facundowehrli/car/service/Controller.java
@@ -5,9 +5,9 @@ import com.facundowehrli.car.persistence.exceptions.NonexistentEntityException;
 import java.util.List;
 
 public class Controller {
-
+    
     PersistenceController controlPersis = new PersistenceController();
-
+    
     public void addCar(String model, String brand, String engine, String licencePlate, String color, int doorCount) {
         //create a new Car befor saving it on persistence layer
         Car car = new Car();
@@ -17,16 +17,31 @@ public class Controller {
         car.setColor(color);
         car.setLicencePlate(licencePlate);
         car.setDoorCount(doorCount);
-
+        
         controlPersis.addCar(car);
     }
-
+    
     public List<Car> getCars() {
         return controlPersis.getCars();
     }
-
+    
     public void deleteCar(int idCar) throws NonexistentEntityException {
         controlPersis.deleteCar(idCar);
     }
-
+    
+    public Car getCar(int idCar) {
+        return controlPersis.getCar(idCar);
+    }
+    
+    public void editCar(Car car, String model, String brand, String engine, String licencePlate, String color, int doorCount) {
+        car.setModel(model);
+        car.setBrand(brand);
+        car.setColor(color);
+        car.setDoorCount(doorCount);
+        car.setEngine(engine);
+        car.setLicencePlate(licencePlate);
+        
+        controlPersis.editCar(car);
+    }
+    
 }
